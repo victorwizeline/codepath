277dd7ecd9b0020fe7981bbdbc8fc90eae62ea0a
Victor Lopez <kikrules@gmail.com>
2017-03-03 01:20:04 -0600

    Activities added

--- app/src/main/java/com/codepath/simpletodo/activities/EditItemActivity.java
+++ app/src/main/java/com/codepath/simpletodo/activities/EditItemActivity.java
@@ -1,13 +1,15 @@
-package com.codepath.simpletodo;
+package com.codepath.simpletodo.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.codepath.simpletodo.R;
+
+public class EditItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_edit_item);
     }
 }
---
+++ app/src/main/java/com/codepath/simpletodo/activities/TodoActivity.java
@@ -0,0 +1,34 @@
+package com.codepath.simpletodo.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.codepath.simpletodo.R;
+
+public class TodoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_todo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
